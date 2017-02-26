6b26fb815919ea6f8a7564ce53cc52727498d02a
oakis <oakismen@gmail.com>
2017-02-26 10:38:04 +0100

    select starting eleven, new clothes with ability to change colors

--- app/components/startingEleven/StartingElevenContainer.js
+++ app/components/startingEleven/StartingElevenContainer.js
@@ -1,12 +1,13 @@
 // Dependencies
 import React, { Component } from 'React';
 import { connect } from 'react-redux';
-import { Alert, ScrollView, View, Image, Text } from 'react-native';
+import { Alert, View, Image, Modal, ScrollView } from 'react-native';
 import { goToRoute } from '../../actions/routeActions';
 import { saveEleven } from '../../actions/gameActions';
 import autobind from 'autobind-decorator';
 // Components
 import UpdateDelete from '../../common/UpdateDelete';
+import PlayerItem from '../../common/PlayerItem';
 import Player from './Player';
 // Styles
 import { objects } from '../../themes';
@@ -16,11 +17,16 @@ class StartingElevenContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startingEleven: [...this.props.game.players],
+      startingEleven: [ ...this.props.game.players ],
       imageSize: null,
-      system: '4-4-2',
+      system: {
+        name: '4-4-2',
+        positions: ['GK','LCB','RCB','LB','RB','LCM','RCM','LM','RM','LST','RST']
+      },
       shirtColor: '#A3132A',
-      shortsColor: '#172347'
+      shortsColor: '#172347',
+      modalVisible: false,
+      pickingPosition: null
     };
   }
   componentWillMount() {
@@ -31,13 +37,16 @@ class StartingElevenContainer extends Component {
     }
   }
   checkPlayer(id) {
-    const {startingEleven} = this.state;
+    const { startingEleven } = this.state;
+    const { players } = this.props;
+    const position = this.state.pickingPosition;
     const i = startingEleven.indexOf(id);
-    const newArr = (i + 1)  ? [...startingEleven.slice(0,i),...startingEleven.slice(i + 1)] : [...startingEleven,id];
-    this.setState({ startingEleven: newArr });
+    const currentPlayer = players.filter((player) => player._id === id);
+    const newArr = (i + 1)  ? [ ...startingEleven.slice(0, i), ...startingEleven.slice(i + 1)] : [ ...startingEleven, { id, name: currentPlayer[0].name, position } ];
+    this.setState({ startingEleven: newArr, modalVisible: false, pickingPosition: null });
   }
   removeAllFromEleven() {
-    this.setState({startingEleven:[]});
+    this.setState({ startingEleven: [] });
   }
   saveEleven() {
     const getFullInfoOnEleven = this.props.players.filter(player => this.state.startingEleven.indexOf(player._id) >= 0 );
@@ -61,6 +70,26 @@ class StartingElevenContainer extends Component {
       imageSize: e.nativeEvent.layout
     });
   }
+  pickPlayer(pos) {
+    this.setState({
+      modalVisible: true,
+      pickingPosition: pos
+    });
+  }
+  playerAdded(pos) {
+    const { startingEleven } = this.state;
+    let result = '';
+    if (startingEleven.length > 0) {
+      startingEleven.forEach((player) => {
+        if (player.position === pos) {
+          const shortName = player.name.split(' ');
+          shortName[1] = shortName[1].substring(0, 1);
+          result = shortName.join(' ');
+        }
+      });
+    }
+    return result;
+  }
   getPlayers() {
     const { system, imageSize, shirtColor, shortsColor } = this.state;
     if (imageSize === null) {
@@ -68,34 +97,48 @@ class StartingElevenContainer extends Component {
     } else {
       return (
         <View>
-          <Player name="Jimmy" position="GK" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Anton" position="LCB" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Martin" position="RCB" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Johan" position="LB" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Shivan" position="RB" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Christian" position="LCM" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Magnus" position="RCM" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Linus" position="LM" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Olle" position="RM" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Markus" position="LST" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
-          <Player name="Andreas" position="RST" system={system} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />
+          {system.positions.map((pos,i) => {
+            const name = this.playerAdded(pos);
+            return <Player onPress={this.pickPlayer} key={i} name={name} position={pos} system={system.name} size={imageSize} shirtColor={shirtColor} shortsColor={shortsColor} />;
+          })}
         </View>
       );
     }
   }
   render() {
-    const {players} = this.props;
-    const {startingEleven} = this.state;
-    return (
-      <Image source={require('../../images/field.png')} style={[objects.screen.field]} onLayout={(e) => this.getImageSize(e) }>
-        {this.getPlayers()}
-      </Image>
-    );
+    const { players } = this.props;
+    const { startingEleven } = this.state;
+    let playerList = [];
+    players.forEach(function(e) {
+        if (!startingEleven.some(s => s.name === e.name)) {
+            playerList.push(e);
+        }
+    });
+    if (this.state.modalVisible) {
+      return (
+        <Modal
+          animationType={'slide'}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => console.log('modal closed')}
+        >
+          <ScrollView style={{ flex: 1 }}>
+            {playerList.map((player,i) => <PlayerItem key={i} index={i} player={player} onPress={this.checkPlayer} />)}
+          </ScrollView>
+        </Modal>
+      );
+    } else {
+      return (
+        <Image source={require('../../images/field.png')} style={[objects.screen.field]} onLayout={(e) => this.getImageSize(e) }>
+          {this.getPlayers()}
+        </Image>
+      );
+    }
   }
 }
 
 function mapStateToProps(state,ownProps) {
-  const {players} = state;
+  const { players } = state;
   const game = state.games.find(g => g._id === ownProps.id);
   return {
     players,
