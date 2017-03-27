daf04a1eb696a90efe05c32843e9987ef8623e0c
oakis <oakismen@gmail.com>
2017-03-27 11:36:28 +0200

    added LoadingSpinner to auto login

--- app/index.js
+++ app/index.js
@@ -2,11 +2,12 @@
 import React, {Component} from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import { View, Text, AsyncStorage } from 'react-native';
+import { View, AsyncStorage } from 'react-native';
 import {persistStore} from 'redux-persist';
 // Containers, Components
 import LoginHandler from './LoginHandler';
 import AppStatusBar from './components/statusBar/StatusBar';
+import LoadingSpinner from './LoadingSpinner';
 // Styles
 import {colors} from './themes';
 
@@ -40,7 +41,7 @@ class App extends Component {
     if (!this.state.rehydrated) {
       return (
         <View style={{flex:1}}>
-          <Text>Loading</Text>
+          <LoadingSpinner loading />
         </View>
       );
     }
